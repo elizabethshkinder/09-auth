a89f871892f06715e2c7a429dc0baaaa8c6bf38b
Elizabeth Shkinder <elizabethshkinder@unknown.home>
2026-02-03 13:02:16 +0000

    set up structure

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -2,7 +2,7 @@
 
 import css from "./NotePreview.module.css";
 import Modal from "@/components/Modal/Modal";
-import { fetchNoteById } from "@/lib/api";
+import { fetchNoteById } from "@/lib/api/api";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
