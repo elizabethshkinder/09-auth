e4cdbcb54eec7067636001e054b3b3e0f72edced
Elizabeth Shkinder <elizabethshkinder@unknown.home>
2026-02-06 22:20:04 +0000

    fix header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import css from "./Header.module.css";
 
 import AuthNavigation from "@/components/AuthNavigation/AuthNavigation";
-
+import { useAuthStore } from "@/lib/store/authStore";
 
 export default function Header() {
+  const user = useAuthStore((s) => s.user);
+
   return (
     <header className={css.header}>
       <Link href="/" aria-label="Home">
@@ -16,6 +20,13 @@ export default function Header() {
           <li>
             <Link href="/">Home</Link>
           </li>
+
+          {user ? (
+            <li>
+              <Link href="/notes/filter/all">Notes</Link>
+            </li>
+          ) : null}
+          
           <AuthNavigation />
         </ul>
       </nav>
